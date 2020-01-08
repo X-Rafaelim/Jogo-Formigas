3b1655bd4e68d2f69a5a3ee2216d95dc090b34d5
Rafael Barcelos <rafa.b.000@gmail.com>
2020-01-08 22:12:05 -0100

    Primeiros paços

---
+++ AiFormiga.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class AiFormiga : MonoBehaviour
+{
+    
+    public float speed;
+
+    private Transform target;
+    
+
+    void Start()
+    {
+        target = GameObject.Find("Recolhedora").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
---
+++ DestroyObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collision name =" + col.gameObject.name);
+        if (col.gameObject.name == "Recolhedora")
+        {
+            Destroy(col.gameObject);
+        }
+
+    }
+
+}
