e20f1f76e2b5723b21cf409bc754bfef7dd7d26c
Rafael Barcelos <rafaelteixeirabarcelos@enta.pt>
2020-01-10 15:02:07 -0100

    Cenas

---
+++ RandomRespawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRespawn : MonoBehaviour
+{
+
+
+    public GameObject box;
+    float randx;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextSpawn = Time.time + spawnRate;
+        randx = Random.Range(-3.24f, 2.89f);
+        whereToSpawn = new Vector2(randx, transform.position.y);
+        Instantiate(box, whereToSpawn, Quaternion.identity);
+        
+    }
+
+
+}
