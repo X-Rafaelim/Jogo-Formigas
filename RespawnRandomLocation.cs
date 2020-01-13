5e3845667fdddc15d912c98c0315d9725d4e9e5b
Rafael Barcelos <rafa.b.000@gmail.com>
2020-01-12 23:46:16 -0100

    Sempre a evoluir

--- RespawnRandomLocation.cs
+++ RespawnRandomLocation.cs
@@ -11,15 +11,30 @@ public class RespawnRandomLocation : MonoBehaviour
      private  float _yAxis;
      private Vector2 _randomPosition ;
      public bool _canInstantiate;
+     private float timer = 0;
+     public float spawnTime = 2f;
+     public float destroyTime = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
          SetRanges();
+         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        timer += Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            Instantiate(formiga, _randomPosition , Quaternion.identity);
+            timer = 0;
+        }
+
+        
+
          _xAxis = UnityEngine.Random.Range(Min.x, Max.x);
          _yAxis = UnityEngine.Random.Range(Min.y, Max.y);
          
@@ -33,11 +48,5 @@ public class RespawnRandomLocation : MonoBehaviour
          Min = new Vector2(19.7f, -22f); //Random value.
          Max = new Vector2(-20.7f, 22.8f); //Another ramdon value, just for the example.
      }
-     private void InstantiateRandomObjects()
-     {
-         if (_canInstantiate)
-         {
-             Instantiate(formiga, _randomPosition , Quaternion.identity);
-         }
 }
-}
+
